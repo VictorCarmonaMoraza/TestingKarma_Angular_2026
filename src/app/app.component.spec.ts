045d586b27d5cc64ba5de41ebbdf8423921f8b0b
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-02 20:27:35 +0100

    Evaluar clases y atributos HTML

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -65,4 +65,39 @@ describe('AppComponent', () => {
     //Queremos saber que el router-outlet existe y no es null
     expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
+
+  //EVALUAR CLASES Y ATRIBUTOS HTML
+  it('should render router-outlet warpped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+
+    console.log(divElement?.classList.value)
+
+    //Evaluamos que nuestro div no sea nulo
+    expect(divElement).not.toBeNull();
+
+    // divElement?.classList.forEach((className) => {
+    //   expect(mustHaveClasses).toContain(className);
+    // });
+
+    const divClasses = divElement?.classList.value.split(' ');
+
+    //Si añadimos clases nuevas o las cmabiamos de poscion esta prueba no falla
+    mustHaveClasses.forEach(className => {
+      expect(divClasses).toContain(className);
+    })
+  });
+
+  //Comprueba que el <a> en sub atributo tiene title ='Buy me a beer'</a>
+  it("should contain the 'buy me a beeer' link", () => {
+    const aElement = compiled.querySelector('a');
+    expect(aElement).not.toBeNull();
+    //comprobamos que la etiqueta title existe dentro de a
+    expect(aElement?.title).not.toBeNull()
+    //Comporbar que el atributo title dentro de la etiqueta
+    expect(aElement?.title).toBe('Buy me a beer');
+    //Buscamos el atributo title dentro de la etiqueta
+    expect(aElement?.getAttribute('title')).toBe('Buy me a beer');
+  })
+
 });
