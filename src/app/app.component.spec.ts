5248b2e9a9a31d2b2b43ab15ba72cfaea308f425
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-02 19:37:43 +0100

    Mi primera prueba

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+//Describe es un simple agrupador de pruebas
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -8,22 +9,50 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  //Esta prueba nos dice si nuestro componente se lleva a montar
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  // it(`should have the 'zoneless-calculator' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('zoneless-calculator');
-  // });
+  it("should be 3", () => {
+    //A =Arrange
+    const num1 = 1;
+    const num2 = 2;
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  // });
+    //A=Act
+    const result = num1 + num2;
+
+    //A=Asserts
+    // if (result !== 3) {
+    //   throw new Error('No es 3')
+    // }
+
+    //Nueva forma Asserts
+    expect(result).toBe(3);
+
+  })
+
+  it(`should have the 'zoneless-calculator' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    //Esperamos que el titulo que tenemos en el componente sea igual a "zoneless-calculator"
+    expect(app.title).toEqual('zoneless-calculator');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled)
+    //Solo evalua la primera
+    // expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+    //Mira si estan las etiquetas entre ellas
+    expect(
+      Array.from(compiled.querySelectorAll('h1'))
+        .some(h1 => h1.textContent?.includes('Hello, zoneless-calculator'))
+    ).toBe(true);
+
+  });
 });
