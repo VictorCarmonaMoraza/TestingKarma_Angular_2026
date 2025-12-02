ece894686bb8892f808002e0b3153173dac0157c
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-02 20:00:25 +0100

    Evaluar RouterOutlet y refactorizacion

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 //Describe es un simple agrupador de pruebas
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>
+  let compiled: HTMLElement
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    //Hacemos la asignacion
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   //Esta prueba nos dice si nuestro componente se lleva a montar
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -35,16 +42,14 @@ describe('AppComponent', () => {
   })
 
   it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     //Esperamos que el titulo que tenemos en el componente sea igual a "zoneless-calculator"
     expect(app.title).toEqual('zoneless-calculator');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    //fixture.detectChanges();
+    //Nos permite obtener el html
     console.log(compiled)
     //Solo evalua la primera
     // expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
@@ -54,5 +59,10 @@ describe('AppComponent', () => {
         .some(h1 => h1.textContent?.includes('Hello, zoneless-calculator'))
     ).toBe(true);
 
+  });
+
+  it('should render router-outlet', () => {
+    //Queremos saber que el router-outlet existe y no es null
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
